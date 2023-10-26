91cef632d860caae4747632896cb18cac0532809
Ranger-HK <maxravindu99@gmail.com>
2023-10-26 14:08:45 +0530

    first commit

--- src/components/Login/TextField/index.jsx
+++ src/components/Login/TextField/index.jsx
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import {styleSheeets} from "./../TextField/styles";
 import {withStyles} from "@mui/styles";
+import {Typography} from "@mui/material";
 
 class TextField extends Component{
     constructor(props) {
@@ -12,6 +13,13 @@ class TextField extends Component{
         const {classes} = this.props
         return(
             <div className={classes.container}>
+                 <div >
+                     <Typography variant="h3" gutterBottom>
+                        Login
+                     </Typography>
+
+                     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                 </div>
 
             </div>
         )
--- src/components/Login/TextField/styles.js
+++ src/components/Login/TextField/styles.js
@@ -1,5 +1,11 @@
 export const styleSheeets = {
     container:{
-
+        display:"flex",
+        flexWrap:"wrap",
+        height:"auto",
+        flexDirection:"column",
+        alignItems:"center",
+        //underline:'always',
+        //backgroundColor:"gray"
     }
 }
